c84f54d737ad943b32dba0246f44014d19a584c8
bonganimasemola <bonganimasemola@icloud.com>
2023-09-22 22:42:57 +0300

    completed phase 1 scope lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+window.customerName = 'bob';
+
+function upperCaseCustomerName() {
+ window.customerName = window.customerName.toUpperCase();
+}
+
+// window.bestCustomer = 'not bob'
+
+function setBestCustomer() {
+  window.bestCustomer = 'not bob';
+}
+
+ // bestCustomer = 'not bob'
+
+function overwriteBestCustomer(newBestCustomer) {
+   window.bestCustomer = newBestCustomer;   
+}
+
+// const  leastFavoriteCustomer = 'other bob';
+
+function changeLeastFavoriteCustomer() {
+    try {
+        const leastFavoriteCustomer = 'start value';
+        leastFavoriteCustomer = 'new value'; 
+    } catch (error) {
+        throw new Error('Assignment to constant variable.');
+    }
+}
+try {
+    changeLeastFavoriteCustomer();
+} catch (error) {
+    console.log(error.message); 
+}
